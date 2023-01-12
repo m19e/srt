09fd6b888b82d7b2c345cd858c2baa66cbb8910b
m19e <nexus.me7290@gmail.com>
2023-01-12 19:31:15 +0900

    style: update dropzone for .srt editor

--- src/components/molecules/Dropzone.tsx
+++ src/components/molecules/Dropzone.tsx
@@ -13,7 +13,7 @@ export const Dropzone = ({ onDrop }: Props) => {
     },
     [onDrop]
   )
-  const { getRootProps, getInputProps } = useDropzone({
+  const { getRootProps, getInputProps, acceptedFiles } = useDropzone({
     onDrop: handleDrop,
     accept: {
       subtitle: [".srt"],
@@ -24,18 +24,18 @@ export const Dropzone = ({ onDrop }: Props) => {
   return (
     <div
       {...getRootProps()}
-      className="flex flex-col items-center p-2 w-full max-w-sm h-40 card bg-base-200 rounded-box"
+      className="flex flex-col items-center p-2 w-full h-40 bg-white rounded-lg border-2 border-gray-100 hover:border-purple-800 transition-colors card"
     >
-      <div className="flex flex-col justify-center items-center w-full h-full text-sm font-black border-2 border-dashed sm:text-base border-base-300 text-base-content rounded-box">
+      <div className="flex flex-col justify-center items-center w-full h-full text-sm sm:text-base text-base-content rounded-box">
         <input {...getInputProps()} />
-        <div className="pb-2">
+        <div className="flex flex-col gap-2 items-center my-2 text-violet-800">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
             strokeWidth={1.5}
             stroke="currentColor"
-            className="w-6 h-6"
+            className="w-8 h-8"
           >
             <path
               strokeLinecap="round"
@@ -43,11 +43,15 @@ export const Dropzone = ({ onDrop }: Props) => {
               d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3"
             />
           </svg>
+          <p className="font-semibold">
+            {acceptedFiles.length
+              ? acceptedFiles[0].name
+              : "Select a file or drag it here"}
+          </p>
+          <div className="py-2 px-4 bg-gradient-to-tr from-purple-600 to-purple-900 rounded-lg">
+            <span className="font-semibold text-white">Select File</span>
+          </div>
         </div>
-
-        <p>字幕ファイルをドロップ</p>
-        <p className="text-sm text-base-content text-opacity-60">または</p>
-        <p>クリックして選択</p>
       </div>
     </div>
   )
